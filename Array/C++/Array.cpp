b8f1e26468e7985cc8c2e1556bc5968eb0c4f852
Shayan Aryania <56693370+ShayanAryania@users.noreply.github.com>
2022-02-23 20:14:50 +0330

    Add files via upload

---
+++ Array/C++/Array.cpp
@@ -0,0 +1,11 @@
+//  Array
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int arr[10,20,30,40,50,60,70,80,90,100];
+    cout<<arr
+    return 0;
+}
---
+++ Array/Python/Array.py
@@ -0,0 +1,8 @@
+# Cretated By Shayan 
+#  Array
+
+import numpy as np
+
+a = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
+print(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9])
